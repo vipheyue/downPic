bfb1dfd606632c28529235d580b4df1ee8a565e7
vipheyue <vipheyue@foxmail.com>
2018-01-23 20:15:15 +0800

    ...

--- quotesbot/spiders/doutu.py
+++ quotesbot/spiders/doutu.py
@@ -4,6 +4,9 @@ import re
 import scrapy
 import os
 
+from quotesbot.spiders.file_item import FileItem
+
+
 class DouTu(scrapy.Spider):
     name = "doutu"
 
@@ -13,29 +16,40 @@ class DouTu(scrapy.Spider):
             # 'http://quotes.toscrape.com/page/2/',
         ]
         # for url in urls:
-            # yield scrapy.Request(url=url, callback=self.parse)
+        # yield scrapy.Request(url=url, callback=self.parse)
 
         # for index in range(1,10000):
         #     print(str(index))
         #     questurl=str('http://api.jiefu.tv/app2/api/dt/item/getDetail.html?itemId=%d'%index)
         #     yield scrapy.Request(url=questurl, callback=self.parse)
-        dirs = os.listdir('/Volumes/Untitled 1/tessss/')
+        dirs = os.listdir('/Volumes/Untitled/doutu/')
         for file in dirs:
             print(file)
-            with open('/Volumes/Untitled 1/tessss/%s'%file, 'r') as f:
+            with open('/Volumes/Untitled/doutu/%s' % file, 'r') as f:
                 # data = json.load(f)
-                data=f.readlines()
+                data = f.readlines()
                 print(data)
-                # reg = r"(http\S+(png|gif|jpg$))"
-                # resultList = re.findall(re.compile(reg), str(data))
-                resultList = re.findall(r'/^http\S+jpg', str(data))
-                print('----'*40)
+                reg = r'(http\S+?(jpg|png|gif))'
+                resultList = re.findall(reg, str(data))
+                print('----' * 40)
                 print(resultList)
+                try:
+                    url=resultList[0][0]
+                except IndexError:
+                    pass
+                else:
+                    yield scrapy.Request(url=url, callback=self.parse)
                 # print(resultList[0])
-                print('****'*40)
-
 
     def parse(self, response):
+        image_item = FileItem()
+        deal_urls = list()
+        deal_urls.append(response.url)
+        print(deal_urls)
+        image_item['file_urls'] = deal_urls
+        image_item['files'] = deal_urls
+        yield image_item
+        pass
         pass
     # def parse(self, response):
     #     os.chdir('/Volumes/Untitled1/doutu/')
